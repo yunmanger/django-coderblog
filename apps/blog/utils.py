02f648ff691a4909f52a815f3ea0114b99abb906
German Ilyin <germanilyin@gmail.com>
2010-06-29 16:16:59 +0300

    yvision bug

--- apps/blog/utils.py
+++ apps/blog/utils.py
@@ -5,15 +5,17 @@ from BeautifulSoup import BeautifulSoup, Comment
 from django_yvi import post as do_post
 
 def check(name, value):
-    if name.upper() == 'HREF' and not value.upper().startswith('HTTP'):
+    if (name.upper() == 'HREF' or name.upper() == 'SRC') and not value.upper().startswith('HTTP'):
         value = "%s%s" % (settings.DOMAIN, value)
         return name, value
     return name, value
         
 def post_to_yvi(post):
     string = render_to_string("blog/post-render.html", {'object': post})
-    soup = BeautifulSoup(string, smartQuotesTo="html") 
+    soup = BeautifulSoup(string, selfClosingTags=['img']) 
     for tag in soup.findAll('a'):
+        tag.attrs = [check(attr, val) for attr, val in tag.attrs]
+    for tag in soup.findAll('img'):
         tag.attrs = [check(attr, val) for attr, val in tag.attrs]
     string = soup.renderContents() 
     do_post(post.title, string ,settings.YVI_LOGIN, settings.YVI_PASSWORD, settings.YVI_USER_ID)
